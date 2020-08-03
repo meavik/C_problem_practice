30ea182736e4dacfd7d6035064bfb01c87bfc5e1
meavik <33549886+meavik@users.noreply.github.com>
2020-08-04 01:33:39 +0600

    Update 1.generateMultiplicationTable.c

--- simpleProblem/1.generateMultiplicationTable.c
+++ simpleProblem/1.generateMultiplicationTable.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
+
 int main(void){
 
     int i,n;
     printf("Enter the number to generate multiplication table: ");
     scanf("%d",&n);
+    printf("Multiplication table of %d is:\n",n);
     for(i = 1; i< 11; i++){
-        printf("%d * %d = %d\n",i,n,i*n);
+        printf("%d X %d = %d\n",i,n,i*n);
     }
 
     return 0;
