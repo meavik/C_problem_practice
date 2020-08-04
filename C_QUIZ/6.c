72461b8353bdf87a97573e86fe84fca8df2bb5f9
meavik <33549886+meavik@users.noreply.github.com>
2020-08-04 20:36:16 +0600

    Create 6.c

---
+++ C_QUIZ/6.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(void){
+
+    char ch;
+    if(ch = printf(" "))
+        printf("It matters.");
+    else
+        printf("It does not matter.");
+
+    return 0;
+}
+/*What is the output?
+1.It matters.
+2.It does not matter.
+3.matters.
+4.No output
+*/
