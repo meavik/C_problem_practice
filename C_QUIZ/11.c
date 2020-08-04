0bf45a882dd1670b9eaf349dbf4cfd1ee0e015ac
meavik <33549886+meavik@users.noreply.github.com>
2020-08-04 21:54:36 +0600

    Create 11.c

---
+++ C_QUIZ/11.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#define square(x) x*x
+
+int main(){
+
+    printf("\nSquare = %d",square(10+2));
+
+    return 0;
+}
+/*What is the output?
+1.Square = 144
+2.Square = 32
+3.Square = 122
+4.Square = 12
+*/
