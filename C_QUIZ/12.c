cd1422201e32216f23397d5afbf77122f634479e
meavik <33549886+meavik@users.noreply.github.com>
2020-08-04 22:06:59 +0600

    Create 12.c

---
+++ C_QUIZ/12.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+
+    int x;
+    x = printf("I see, Sea in C");
+    printf("\n x = %d",x);
+
+    return 0;
+}
+/*What is the output?
+1.x = 15
+2.x = 12
+3.Garbage value
+4.Error
+*/
