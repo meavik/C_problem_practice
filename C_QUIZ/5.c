d1c6e5c9a674c5bd5cd29d491f16688962420582
meavik <33549886+meavik@users.noreply.github.com>
2020-08-04 20:33:48 +0600

    Create 5.c

---
+++ C_QUIZ/5.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(void){
+
+    int x = 3;
+    float y = 3.0;
+    if(x == y)
+        printf("X and Y are equal");
+    else
+        printf("X and Y are not equal");
+
+    return 0;
+}
+/*What is the output?
+1.X and Y are equal
+2.X and Y are not equal
+3.unpredictable
+4.No output
+*/
