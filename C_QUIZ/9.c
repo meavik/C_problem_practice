8aaac98f05d4946fdfd3b33a6a5089ebe09f278e
meavik <33549886+meavik@users.noreply.github.com>
2020-08-04 21:01:22 +0600

    Create 9.c

---
+++ C_QUIZ/9.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main(){
+
+    int x[]={10,20,30,40,50};
+    printf("\n%d %d %d %d 5d",x[4],x[3],x[2],x[1]x[0]);
+}
+
+/*What is the output?
+1.10 20 30 40 50
+2.50 40 30 20 10
+3.error
+4.None of these
+*/
