efcc3ce0673b9e7d476a82488ccf97f2c39b668f
meavik <33549886+meavik@users.noreply.github.com>
2020-08-08 22:10:01 +0600

    Create 21.c

---
+++ C_QUIZ/21.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+
+    int c = 2, d = 5, e = 10,a;
+    a = c>1?d>1||e>1?100:200 :
+    printf("\na = %d",a);
+
+    return 0;
+}
+/*
+What is the output?
+1.a = 300
+2.a = 100
+3.a = 200
+4.none of these
+*/
