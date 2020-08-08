0b9afd99c0475647486cd2aeebaeb8106244928b
meavik <33549886+meavik@users.noreply.github.com>
2020-08-08 22:31:11 +0600

    Create 23.c

---
+++ C_QUIZ/23.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+
+    float f = 0.1;
+    if(f == 0.1)
+        printf("Yes\n");
+    else
+        printf("No\n");
+
+    return 0;
+}
+/*
+What will be the output?
+1.NO
+2.Error
+3.No output
+4.YES
+*/
