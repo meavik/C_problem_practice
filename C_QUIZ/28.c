8649c6924d457d188b9a9a2ec7f2efea642a6561
meavik <33549886+meavik@users.noreply.github.com>
2020-08-08 23:13:48 +0600

    Create 28.c

---
+++ C_QUIZ/28.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+static int i;
+
+int main(){
+
+    int i;
+    printf("\ni is %d",i);
+    return 0;
+}
+/*
+What will be the output?
+1.0
+2.garbage value
+3.1
+4.runtime error
+*/
