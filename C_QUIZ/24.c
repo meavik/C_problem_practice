074c9183a1ba45204c81b1051e19e1f1b492f0ec
meavik <33549886+meavik@users.noreply.github.com>
2020-08-08 22:36:04 +0600

    Create 24.c

---
+++ C_QUIZ/24.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+
+    int i;
+    for(i = 0; i-5;i--);
+    {
+        printf("Hello");
+    }
+
+    return 0;
+}
+/*
+How many times Hello will be printed?
+1.Compilation Error
+2.1
+3.4
+4.Runtime Error
+*/
